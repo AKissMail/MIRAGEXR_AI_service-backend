e473abf79d1c0d02c90b6cedc72208c9ccf365d7
321501 <KLNotbf22xMrt5VzFSAwdkmSbH+--+>
2024-02-22 14:11:15 +0100

    End point speak is done and tested \o/

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
-    'products'
+    'products',
+    'speak'
 ]
 
 MIDDLEWARE = [
---
+++ py_client/TTS_Test.py
@@ -0,0 +1,18 @@
+import requests
+'''
+This is a simple Test script to test if the TTS endpoint is working properly. 
+'''
+endpoint = "http://localhost:8000/speak/"  # TTS-Endpoint
+
+data = {"speakOut": "Hi, jeg heter Andreas", "voice": "default", "model": "default", "speed": 0.5}
+response = requests.post(endpoint, json=data)
+
+
+if response.status_code == 200:
+    filename = "TTS_Test.mp3"
+    with open(filename, 'wb') as f:
+        f.write(response.content)
+    print(f"MP3 saved as {filename}.")
+else:
+    print(f"Error: {response.status_code}")
+    print(response.text)
