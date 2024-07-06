2f0c7d5db89f7fff8e641ec8169abcd436f097e7
Andreas Kissmehl <69756846+AKissMail@users.noreply.github.com>
2024-07-06 21:47:33 +0200

    Refactor audio handling methods into AudioTransformator class.
    The code responsible for handling binary data and converting wav files to mp3 was moved from views.py to a dedicated class, AudioTransformator. This change leads to better organization of the codebase by structuring related functionalities into a single class. It also improves code reusability since these audio transformation utilities can be easily accessed from different parts of the application.

--- listen/audio_utility/audio_transformator.py
+++ listen/audio_utility/audio_transformator.py
@@ -2,6 +2,11 @@ import numpy
 import soundfile
 from io import BytesIO
 
+import base64
+import subprocess
+
+from django.core.files.base import ContentFile
+
 
 class AudioTransformator:
     """
@@ -26,3 +31,59 @@ class AudioTransformator:
         if len(data.shape) > 1 and data.shape[1] > 1:
             data = numpy.mean(data, axis=1)
         return data
+
+    @staticmethod
+    def handleBinary(validated_data):  # todo das auch!
+        """
+        Handle binary data.
+
+        This method takes in a dictionary of validated data and performs the following tasks:
+        1. If the 'message' key exists in the validated data dictionary, it reads the first 4 bytes of the message/
+        2. Seeks the message back to the beginning.
+        3. Converts the WAV data in the message to MP3 format using the 'convert_wav_to_mp3' function.
+        4. Base64 encodes the MP3 data.
+        5. Creates a ContentFile object from the base64 decoded MP3 data with the name "demo.mp3".
+        6. Replaces the 'message' key in the validated data dictionary with the content file.
+        7. Writes the content of the content file to a file named "demo.mp3".
+        8. Returns the modified validated data dictionary.
+
+        Parameters:
+            validated_data (dict): A dictionary of validated data.
+
+        Returns:
+            dict: The modified validated data dictionary.
+        """
+        if validated_data['message']:
+            header = validated_data['message'].read(4)
+            validated_data['message'].seek(0)
+            mp3_data = convert_wav_to_mp3(validated_data['message'])
+            base64_encoded = base64.b64encode(mp3_data).decode()
+            content_file = ContentFile(base64.b64decode(base64_encoded), name="demo.mp3")
+            validated_data['message'] = content_file
+            with open("demo.mp3", 'wb+') as destination:
+                for chunk in content_file.chunks():
+                    destination.write(chunk)
+            return validated_data
+
+    @staticmethod
+    def convert_wav_to_mp3(wav_input):  # todo das gehört in die Audio ufillitys !
+        """
+        Converts a WAV audio file to an MP3 audio file using FFmpeg.
+        """
+        command = [
+            'ffmpeg',
+            '-i', '-',
+            '-acodec', 'libmp3lame',
+            '-f', 'mp3',
+            'pipe:1'
+        ]
+
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate(input=wav_input.read())
+
+        if process.returncode != 0:
+            error_message = stderr.decode()
+            print(f"FFmpeg error: {error_message}")
+            raise Exception(f"FFmpeg error: {error_message}")
+
+        return stdout
--- listen/views.py
+++ listen/views.py
@@ -1,7 +1,3 @@
-import base64
-import subprocess
-
-from django.core.files.base import ContentFile
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -9,63 +5,11 @@ from rest_framework import status
 
 from .serializers import ListenSerializer
 from listen.models.whisperNBAiLab import whisper_nb_ai_lab
-from listen.models.whisperOpenAI import whisper_open_ai_remote
+from listen.models.whisperOpenAI import whisper_open_ai_remote, wisper_open_ai_local
+from listen.audio_utility.audio_transformator import AudioTransformator
 
 
-def handleBinary(validated_data):
-    """
-    Handle binary data.
 
-    This method takes in a dictionary of validated data and performs the following tasks:
-    1. If the 'message' key exists in the validated data dictionary, it reads the first 4 bytes of the message/
-    2. Seeks the message back to the beginning.
-    3. Converts the WAV data in the message to MP3 format using the 'convert_wav_to_mp3' function.
-    4. Base64 encodes the MP3 data.
-    5. Creates a ContentFile object from the base64 decoded MP3 data with the name "demo.mp3".
-    6. Replaces the 'message' key in the validated data dictionary with the content file.
-    7. Writes the content of the content file to a file named "demo.mp3".
-    8. Returns the modified validated data dictionary.
-
-    Parameters:
-        validated_data (dict): A dictionary of validated data.
-
-    Returns:
-        dict: The modified validated data dictionary.
-    """
-    if validated_data['message']:
-        header = validated_data['message'].read(4)
-        validated_data['message'].seek(0)
-        mp3_data = convert_wav_to_mp3(validated_data['message'])
-        base64_encoded = base64.b64encode(mp3_data).decode()
-        content_file = ContentFile(base64.b64decode(base64_encoded), name="demo.mp3")
-        validated_data['message'] = content_file
-        with open("demo.mp3", 'wb+') as destination:
-            for chunk in content_file.chunks():
-                destination.write(chunk)
-        return validated_data
-
-
-def convert_wav_to_mp3(wav_input):
-    """
-    Converts a WAV audio file to an MP3 audio file using FFmpeg.
-    """
-    command = [
-        'ffmpeg',
-        '-i', '-',
-        '-acodec', 'libmp3lame',
-        '-f', 'mp3',
-        'pipe:1'
-    ]
-
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate(input=wav_input.read())
-
-    if process.returncode != 0:
-        error_message = stderr.decode()
-        print(f"FFmpeg error: {error_message}")
-        raise Exception(f"FFmpeg error: {error_message}")
-
-    return stdout
 
 
 @api_view(['POST'])
@@ -116,7 +60,7 @@ def listen(request):
             if validated_data['model'].strip() == "Norwegian":
                 response_data = whisper_nb_ai_lab(validated_data)
             elif validated_data['model'].strip() == "BinaryWhisperOpenAILocal":
-                response_data = whisper_open_ai_remote(handleBinary(validated_data))
+                response_data = wisper_open_ai_local(AudioTransformator.handleBinary(validated_data))
                 print(response_data)
             else:
                 response_data = whisper_open_ai_remote(validated_data)
